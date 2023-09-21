5b7d46e455abb6599ebd24dcdf7445d53337e010
Rômulo Moitinho <romulo.fer.w@gmail.com>
2023-09-21 16:12:43 -0300

    feat: added the button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import Classes from './index.module.css';
+
+
+
+const InputArea: React.FC = () => {
+
+    return (
+    <div className={Classes.container}>
+        <button type="button" className={Classes.center}>
+            Entrar
+        </button>
+    </div>    
+    ) 
+}
+
+export default InputArea;
--- src/components/LoginElements.tsx/index.tsx
+++ src/components/LoginElements.tsx/index.tsx
@@ -1,6 +1,8 @@
 import Classes from './index.module.css'
 import InputArea from '../InputArea';
+import Button from './../Button'
 import { User, Lock } from '@phosphor-icons/react';
+
 const LoginElements: React.FC = () => {
 
     return (
@@ -8,8 +10,10 @@ const LoginElements: React.FC = () => {
         <h1 className={Classes.title}>Olá,</h1>
         <p className={Classes.text}>Para continuar navegando de forma segura, efetue o login.</p>
         <h2 className={Classes.subTitle}>Login</h2>
-        <InputArea icone={<User />} placeholder={''}></InputArea>
-        <InputArea icone={<Lock  />} placeholder={''}></InputArea>
+        <InputArea icone={<User />} placeholder={'Usuário'}></InputArea>
+        <InputArea icone={<Lock  />} placeholder={'Senha'}></InputArea>
+        <Button/>
+        <p className={Classes.lastText}>Novo por aqui?<a href=""> Registre-se</a></p>
     </div>    
     ) 
 }
