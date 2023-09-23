7e4460bd73ec2a84981cba36a26d4b3304c60ad5
Rômulo Moitinho <romulo.fer.w@gmail.com>
2023-09-23 20:09:56 -0300

    feat: added the log of the values on the input in registe and login

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,25 +1,56 @@
-import { ReactNode } from 'react';
-import Classes from './index.module.css';
+import { ReactNode } from "react";
+import Classes from "./index.module.css";
 
 interface Props {
-    icone: ReactNode;
-    placeholder: string;
-    yellowBorder: boolean;
-  }
-
-const InputArea: React.FC<Props> = ({icone, placeholder, yellowBorder}) => {
-
-    return (
-    <div className={`${Classes.container} ${yellowBorder ? Classes.yellowBorder : ''}`}>
-        <input
-            id="id"
-            type="type"
-            name="name"
-            placeholder={placeholder}
-        />
-        {icone}
-    </div>    
-    ) 
+  icone: ReactNode;
+  placeholder: string;
+  yellowBorder: boolean;
+  minLength?: number;
+  maxLength?: number;
+  type?: string;
+  onChangeData: (data: string) => void;
+  inputRef: React.RefObject<HTMLInputElement>;
+  id: string;
+  name: string;
 }
+
+const InputArea: React.FC<Props> = ({
+  icone,
+  placeholder,
+  yellowBorder,
+  minLength = 0,
+  maxLength = 255,
+  type = "text",
+  onChangeData,
+  inputRef,
+  id,
+  name,
+}) => {
+  const handleChange = () => {
+    // Acessa o valor do input diretamente usando a ref
+    const newValue = inputRef.current?.value || '';
+    onChangeData(newValue);
+  };
+
+  return (
+    <div
+      className={`${Classes.container} ${
+        yellowBorder ? Classes.yellowBorder : ""
+      }`}
+    >
+      <input
+        id={id}
+        type={type}
+        name={name}
+        minLength={minLength}
+        maxLength={maxLength}
+        placeholder={placeholder}
+        ref={inputRef}
+        onChange={handleChange}
+      />
+      {icone}
+    </div>
+  );
+};
 
 export default InputArea;
--- src/components/LoginElements/index.tsx
+++ src/components/LoginElements/index.tsx
@@ -1,24 +1,72 @@
-import { useState } from 'react';
-import Classes from './index.module.css'
-import InputArea from '../InputArea';
-import Button from '../Button'
-import { User, Lock } from '@phosphor-icons/react';
+import { useRef, useState } from "react";
+import Classes from "./index.module.css";
+import InputArea from "../InputArea";
+import Button from "../Button";
+import { User, Lock } from "@phosphor-icons/react";
 
 const LoginElements: React.FC = () => {
-    const userPasswordAlert = false;
-    
-    return (
+  const userPasswordAlert = false;
+  const inputRefUser = useRef<HTMLInputElement | null>(null);
+  const inputRefPassword = useRef<HTMLInputElement | null>(null);
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const userInputChange = (data: string) => {
+    setUser(data);
+  };
+
+  const passwordInputChange = (data: string) => {
+    setPassword(data);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log({user, password})
+  };
+
+  return (
     <div className={Classes.container}>
-        <h1 className={Classes.title}>Olá,</h1>
-        <p className={Classes.text}>Para continuar navegando de forma segura, efetue o login.</p>
-        <h2 className={Classes.subTitle}>Login</h2>
-        <InputArea icone={<User />} placeholder={'Usuário'} yellowBorder={userPasswordAlert}></InputArea>
-        <InputArea icone={<Lock  />} placeholder={'Senha'} yellowBorder={userPasswordAlert}></InputArea>
-        {userPasswordAlert && <p className={Classes.alertText}>Usuário e/ou Senha inválidos.<br/>Por favor, tente novamente!</p>}
-        <Button/>
-        <p className={Classes.lastText}>Novo por aqui?<a href="/register"> Registre-se</a></p>
-    </div>    
-    ) 
-}
+      <h1 className={Classes.title}>Olá,</h1>
+      <p className={Classes.text}>
+        Para continuar navegando de forma segura, efetue o login.
+      </p>
+      <h2 className={Classes.subTitle}>Login</h2>
+        <InputArea
+          id={'user'}
+          name={'user input'}
+          icone={<User />}
+          placeholder={"Usuário"}
+          yellowBorder={userPasswordAlert}
+          onChangeData={userInputChange}
+          inputRef={inputRefUser}
+        ></InputArea>
+
+        <InputArea
+          id={'password'}
+          name={'password input'}
+          icone={<Lock />}
+          placeholder={"Senha"}
+          yellowBorder={userPasswordAlert}
+          minLength={6}
+          maxLength={50}
+          type='password'
+          onChangeData={passwordInputChange}
+          inputRef={inputRefPassword}
+        ></InputArea>
+
+        {userPasswordAlert && (
+          <p className={Classes.alertText}>
+            Usuário e/ou Senha inválidos.
+            <br />
+            Por favor, tente novamente!
+          </p>
+        )}
+        <Button  onSubmit={handleSubmit} text={'Entrar'}/>
+      <p className={Classes.lastText}>
+        Novo por aqui?<a href="/register"> Registre-se</a>
+      </p>
+    </div>
+  );
+};
 
 export default LoginElements;
